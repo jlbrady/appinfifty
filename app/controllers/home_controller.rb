606a4f66fc1fe5efff6f5ec4435a9b3e1df4bb67
James Brady <jlbrady@umich.edu>
2017-07-25 14:19:02 -0400

    tuesday1

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
-    shop = Shop.find_by(shopify_domain: params[:shop])
-    shop = ShopifyApp::SessionRepository.retrieve(shop.id)
-    puts(shop.shopify_token)
   end
 end
