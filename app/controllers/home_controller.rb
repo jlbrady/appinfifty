b4d638d2941f5b42e82c54c392b6e07b85976afe
James Brady <jlbrady@umich.edu>
2017-07-25 16:46:48 -0400

    httparty4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,4 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
   end
-
-  def shopinfo
-    @shop = ShopifyAPI::Shop.current.find(:all, params: { limit: 10 })
-  end
-
 end
