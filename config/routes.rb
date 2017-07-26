fd432cf56b2fabafb01bc3f0a4891e49086ef774
James Brady <jlbrady@umich.edu>
2017-07-26 15:34:23 -0400

    post-crash

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
-  mount ShopifyApp::Engine, at: '/'
+  mount ShopifyApp::Engine, at: '/#/shopifySignUp'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
