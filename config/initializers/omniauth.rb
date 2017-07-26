82677b7adde991d367134306b3a657f0f16cec18
James Brady <jlbrady@umich.edu>
2017-07-26 14:58:07 -0400

    post-crash

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ShopifyApp.configuration.api_key,
     ShopifyApp.configuration.secret,
     scope: ShopifyApp.configuration.scope,
-           callback_path: '/auth/shopify/callback/#/shopifySignUp'
+           callback_path: '/#/shopifySignUp/auth/shopify/callback'
 end
