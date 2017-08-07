effa2d3776af3af125b2c712e6983e10d6463be6
James Brady <jlbrady@umich.edu>
2017-08-07 15:10:55 -0400

    testing

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
   config.api_key = "b392bf79d0f84322938aa67c0476ffed"
   config.secret = "f25cd5a32c006286886ad2cfe1261281"
-  config.scope = "write_script_tags"
+  config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
